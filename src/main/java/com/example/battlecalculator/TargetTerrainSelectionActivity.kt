3ba3416e338454e9dd9c19fb0cae57a6e13f6ad0
Tuomas Ruotsalainen <tuomas.h.ruotsalainen@gmail.com>
2022-10-23 13:15:42 +0300

    UI functions for terran selection

--- src/main/java/com/example/battlecalculator/TargetTerrainSelectionActivity.kt
+++ src/main/java/com/example/battlecalculator/TargetTerrainSelectionActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.util.Log.DEBUG
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import java.util.logging.Logger
@@ -15,6 +16,64 @@ class TargetTerrainSelectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_target_terrain_selection)
+
+        val forestCheck = findViewById<CheckBox>(R.id.checkBoxForest)
+        val plainCheck = findViewById<CheckBox>(R.id.checkBoxPlain)
+        val townCheck = findViewById<CheckBox>(R.id.checkBoxTown)
+        val cityCheck = findViewById<CheckBox>(R.id.checkBoxCity)
+        val swampCheck = findViewById<CheckBox>(R.id.checkBoxSwamp)
+        val minorRiverCheck = findViewById<CheckBox>(R.id.checkBoxMinorRiver)
+        val majorRiverCheck = findViewById<CheckBox>(R.id.checkBoxMajorRiver)
+
+        val defenseWorks1Check = findViewById<CheckBox>(R.id.checkBoxDefenseWorks1)
+        val defenseWorks3Check = findViewById<CheckBox>(R.id.checkBoxDefenseWorks3)
+        val bridgeCheck = findViewById<CheckBox>(R.id.checkBoxBridgeExists)
+
+        plainCheck.isChecked = true
+
+        defenseWorks3Check.setOnClickListener {
+            defenseWorks1Check.isChecked = false
+        }
+
+        defenseWorks1Check.setOnClickListener {
+            defenseWorks3Check.isChecked = false
+        }
+
+        majorRiverCheck.setOnClickListener {
+            if (!majorRiverCheck.isChecked && !minorRiverCheck.isChecked) {
+                bridgeCheck.isChecked = false
+            }
+            if (majorRiverCheck.isChecked) {
+                minorRiverCheck.isChecked = false
+            }
+
+        }
+
+        minorRiverCheck.setOnClickListener {
+            if (!majorRiverCheck.isChecked && !minorRiverCheck.isChecked) {
+                bridgeCheck.isChecked = false
+            }
+
+            if (minorRiverCheck.isChecked) {
+                majorRiverCheck.isChecked = false
+            }
+        }
+
+        bridgeCheck.setOnClickListener {
+            if (!majorRiverCheck.isChecked && !minorRiverCheck.isChecked) {
+                bridgeCheck.isChecked = false
+            }
+        }
+
+        val applyButton = findViewById<Button>(R.id.terrain_apply)
+
+        val intent = Intent(this, GroundCombatActivity::class.java)
+        applyButton.setOnClickListener{
+            //Toast.makeText(this, "Unit type: $radioButtonStr, attack strength: ${strengths.first}, defense strength: ${strengths.second}", Toast.LENGTH_LONG*10).show()
+            startActivity(intent)
+            finish()
+        }
+
 
         /*
         val postureAssault = findViewById<RadioButton>(R.id.radio_posture_assault)
@@ -39,20 +98,14 @@ class TargetTerrainSelectionActivity : AppCompatActivity() {
             }
         }
 
-        val applyButton = findViewById<Button>(R.id.groudcombatApply)
 
-        val intent = Intent(this, DisengagementActivity::class.java)
-        applyButton.setOnClickListener{
-            //Toast.makeText(this, "Unit type: $radioButtonStr, attack strength: ${strengths.first}, defense strength: ${strengths.second}", Toast.LENGTH_LONG*10).show()
-            startActivity(intent)
-            finish()
-        }
 
 
          */
 
 
     }
+
 
 
 }
